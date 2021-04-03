78004d34c1e23aec0cbaba16cd7ddeeeef3c5739
DIAMONDTEETHSAMURAI <80170089+DIAMONDTEETHSAMURAI@users.noreply.github.com>
2021-04-03 12:34:45 -0400

    marks works,
    added an assignments column

--- assignment3/logindatabase.sql
+++ assignment3/logindatabase.sql
@@ -16,17 +16,18 @@ INSERT INTO users VALUES(4, 'instructor2', 'instructor2',1);
 CREATE TABLE IF NOT EXISTS marks (
     id INTERGER NOT NULL,
     name VARCHAR(20) NOT NULL,
-    mark INTERGER CHECK(mark>0) NOT NULL
+    mark INTERGER CHECK(mark>0) NOT NULL,
+    assignment VARCHAR(30) NOT NULL
 );
 
 
-INSERT INTO marks VALUES(1, 'Student1', '50');
-INSERT INTO marks VALUES(1, 'Student1', '100');
+INSERT INTO marks VALUES(1, 'Student1', '50', Midterm);
+INSERT INTO marks VALUES(1, 'Student1', '100', Assignment 1);
 
 CREATE TABLE IF NOT EXISTS afeed (
-    feedback VARCHAR(1000) NOT NULL
-    ainfo VARCHAR(1000)
+    feedback VARCHAR(1000) NOT NULL,
+    ainfo VARCHAR(1000) NOT NULL,    
     instructor VARCHAR(30) NOT NULL
 );
 
-INSERT INTO afeed VALUES ("SUCK UR MUM PUSSY HOLE I HATE THIS CLASS", "none", "Instructor1");
+INSERT INTO afeed VALUES ("SUCK UR MUM PUSSY HOLE I HATE THIS CLASS", 'IM UNDER THE WATER', "Instructor1");
