7bf01d0822b0dbc101dff5122da268b552f816e0
DIAMONDTEETHSAMURAI <80170089+DIAMONDTEETHSAMURAI@users.noreply.github.com>
2021-04-04 20:56:43 -0400

    added javascript
    created pop up for anon feedback and remark request

---
+++ assignment3/static/script.js
@@ -0,0 +1,11 @@
+function anonfeedbackpopup(){
+    alert("Your feedback has been submitted");
+    console.log("hello")
+    
+}
+
+function remarkpopup (){
+    alert("Your remark request has been submitted");
+    console.log("hello")
+   
+}
